5af6c19c5844fd27d9a27baaaf096e6af8682053
hectormorenosoftware <hectormorenosoftware@gmail.com>
2025-09-04 15:02:40 -0500

    Added cors package

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoPractice = require("./mongo");
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static("build"));
 
 app.post("/products", mongoPractice.createProduct);
 
@@ -16,6 +18,6 @@ app.get("/product", mongoPractice.getProduct);
 
 app.delete("/product", mongoPractice.deleteProduct);
 
-app.listen(3000, () => {
-  console.log("listening in port 3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("listening in port 5000");
 });
